7b63d7fd667700bd55811b99ee32e732a3fbb6fb
hyungyoo <hjyoo901112@gmail.com>
2023-02-27 17:03:54 +0100

    feat: Setting configModule and typeOrmModule
    1. Check the env file with configModule and joi
    2. Install typeOrmModule and connect to DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,38 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import * as joi from "joi";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { Users } from "./users/entites/users.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/.env.${process.env.NODE_ENV}`,
+      validationSchema: joi.object({
+        POSTGRES_HOST: joi.string().required(),
+        POSTGRES_PORT: joi.string().required(),
+        POSTGRES_PASSWORD: joi.string().required(),
+        POSTGRES_USERNAME: joi.string().required(),
+        POSTGRES_DB: joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: process.env.ENV !== "prod",
+      logging: false,
+      entities: [],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
