89970e64a7dec760ebb352c9c17c43d117859183
hyungyoo <hjyoo901112@gmail.com>
2023-03-07 17:21:11 +0100

    style: Add and modify comments, error messages and constants
    1. Add comments.
    2. Add constants for error message.

---
+++ backend/src/redis/interfaces/redis.constants.ts
@@ -0,0 +1,13 @@
+/**
+ * When storing and searching access tokens in Redis,
+ * leaving the value field empty makes it faster to search by only looking up the keys since it becomes null.
+ * However, it is difficult to distinguish between null and "", so we made a constant for it.
+ */
+export const ACCESS_TOKEN_VALUE = "";
+
+/**
+ * Const for exception message
+ */
+export const ACCESS_TOKEN_BLACKLISTED =
+  "The access token is blacklisted. Access is denied";
+export const ACCESS_TOKEN_PAYLOAD_ERROR = "Access token has wrong payload";
--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -4,12 +4,13 @@ import { RedisService } from "./redis.service";
 import { JwtService } from "@nestjs/jwt";
 
 /**
- * 보통은 작은 프로젝트에서는 하나의 호스트에서 두 개의 인스턴스를 구성하여
- * 각각의 인스턴스에서 블랙리스트와 캐시를 처리합니다.
- * 이 방식은 호스트 비용을 절약하면서도 충분한 성능을 제공합니다.
- * 하나의 인스턴스에서 여러 데이터베이스를 제공하기 하는것은 필요하지 않은 추가 복잡성을 추가할 수 있습니다.
- * 두 대의 호스트를 사용하는것은 비용 측면에서는 비효율적일 수 있습니다.
- * 하지만 서버간의 분리를 통해 더 높은 안정성과 가용성을 제공할 수 있습니다. 그러나 작은 프로젝트에서는 대개 이러한 높은 안정성과 가용성이 필요하지 않을 수 있습니다.
+ * Usually, in small projects, two instances are configured on a single host, 
+ * and each instance handles the blacklist and cache separately.
+ * This approach provides sufficient performance while saving on hosting costs.
+ * Providing multiple databases on a single instance can add unnecessary complexity.
+ * Using two hosts may be inefficient in terms of cost.
+ * Therefore, two instances are set up for the blacklist and food API cache, respectively, 
+ * with key prefixes added to easily differentiate between them.
  */
 @Global()
 @Module({
